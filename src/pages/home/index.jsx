40dcfdc81565a20d742ec7ff3c0188337c765d46
Matheus Cristhyan <matheuscristhyan.2@gmail.com>
2024-02-10 11:19:13 -0300

    cards da tela inicial adicionados

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,48 @@
-import {  Header  } from '../../components/header';
+import { Header } from '../../components/header';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import { ServiceData } from './constants';
+import { FreeMode, Pagination, Navigation } from 'swiper/modules';
 
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/free-mode";
-
-import { FreeMode, Pagination } from "swiper/modules";
-
-import { RxArrowTopRight } from "react-icons/rx";
-import {ServiceData} from './constants';
+import { FaCircleArrowLeft, FaCircleArrowRight  } from "react-icons/fa6";
 
 export function Home(){
   return(
     <div className="app">
       <Header />
-      <div className="w-full h-screen bg-neutral-900 " />
+      <div className="w-full h-screen flex items-center justify-center"> {/* Adicionando relative para posicionamento absoluto das setas */}
+        <div className="max-w-[36rem] translate-y-[15%]"> {/* Controle de tamanho máximo */}
+
+          <Swiper
+            slidesPerView={2}
+            spaceBetween={50}
+            freeMode={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            }}
+            modules={[FreeMode, Pagination, Navigation]}
+            className="mySwiper"
+          >
+            {ServiceData.map((item, index) => (
+              <SwiperSlide key={index} className="cursor-pointer hover:opacity-80">
+                <img src={`${item.image}`} alt=""/>
+              </SwiperSlide>
+            ))}
+
+          </Swiper>
+
+          {/* Posicionando as setas de navegação */}
+          <div className="swiper-button-prev absolute left-0 top-1/2 transform -translate-y-1/2 z-10 hover:scale-125 duration-100 ease-in">{<FaCircleArrowLeft size={36} color='white' cursor='pointer'/>}</div>
+          <div className="swiper-button-next absolute right-0 top-1/2 transform -translate-y-1/2 z-10 hover:scale-125 duration-100 ease-in">{< FaCircleArrowRight size={36} color='white' cursor= 'pointer'/>}</div>
+
+        </div>
+      </div>
     </div>
   )
 }
