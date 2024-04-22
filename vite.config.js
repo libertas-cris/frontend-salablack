765440d28041a97c603f437b32e0c65612f4233d
Matheus Cristhyan <matheuscristhyan.2@gmail.com>
2024-04-22 13:32:01 -0300

    vercel.json added

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    createHtmlPlugin({
-      publicPath: '/',
-      entry: './main.jsx',
-    }),
-  
-  ],
+  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
